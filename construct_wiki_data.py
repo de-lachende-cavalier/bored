18c0a6b1c3046c4569e62522744fd9ef51680166
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-07-30 12:44:51 +0200

    minor edits and added the snippet extraction part

--- construct_wiki_data.py
+++ construct_wiki_data.py
@@ -1,37 +1,33 @@
-import time
-
 from wiki_tools import *
 
 print("[+] Gathering highly ambiguous entities...")
 
 json_file = "data/highly_ambiguous_entities.json"
-
 highly_ambiguous_entities = load_checkpoint(json_file)
 if highly_ambiguous_entities:
-    print(f"\t[+] Resuming from checkpoint file {json_file}.")
+    print(f"\t[+] Resuming from checkpoint file {json_file}...")
 else:
-    print("\t[+] No checkpoint file found, starting fresh.")
+    print("\t[+] No checkpoint file found, starting fresh...")
 
-# https://www.mediawiki.org/wiki/Wikimedia_REST_API#Terms_and_conditions
-# we'll use 100 reqs/s just to be on the safe side
-rate_limit_interval = 0.01
+print("\t[+] Getting all the disambiguation page titles...")
+disambiguation_page_titles = get_disambiguation_page_titles()
 
-print("\t[+] Getting all the disambiguation pages...")
-disambiguation_pages = get_disambiguation_pages()
+# as of 30/07/2024, all the highly ambiguous entities (threshold 10) make up precisely 11008 entries
+all_ambiguous_in_checkpoint = len(highly_ambiguous_entities) == 11008
+if not all_ambiguous_in_checkpoint:
+    threshold = 10
+    print(f"\t[+] Filtering for the ones with at least {threshold} disambiguations...")
+    filter_out_pages(
+        disambiguation_page_titles, highly_ambiguous_entities, threshold, json_file
+    )
+    print(f"\t[+] Results saved to {json_file}.")
 
-threshold = 10
-print(f"\t[+] Filtering for the ones with at least {threshold} disambiguations...")
-for page_title in disambiguation_pages:
-    if page_title not in highly_ambiguous_entities:
-        link_count = count_links(page_title)
-        if link_count > threshold:
-            if has_alpha(page_title):
-                highly_ambiguous_entities[page_title] = link_count
-                save_checkpoint(json_file, highly_ambiguous_entities)
-        time.sleep(rate_limit_interval)
-
-print(f"\t[+] Results saved to {json_file}.")
+print(f"[+] All the ambiguous entities are already in the checkpoint file.")
 
 print("[+] Gathering text snippets for each entity...")
+
+highly_ambiguous_entities = load_checkpoint(json_file)
+for disambiguation_page in highly_ambiguous_entities:
+    create_snippets(disambiguation_page, num_snippets_per_page=7)
 
 print("[+] All done!")
