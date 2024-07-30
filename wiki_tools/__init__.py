3779f234f0091b4b9cf818b5567f946aa48385e5
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-07-30 12:51:41 +0200

    reorganisation

--- wiki_tools/__init__.py
+++ wiki_tools/__init__.py
@@ -1,6 +1,5 @@
 from .config import WIKI, wiki_rate_limiter
 
-from .ambiguous_pages import *
 from .process_entities import *
 from .utils import *
 from .snippets import *
--- wiki_tools/utils.py
+++ wiki_tools/utils.py
@@ -4,6 +4,24 @@ import os
 from .config import WIKI, wiki_rate_limiter
 
 
+@wiki_rate_limiter
+def get_disambiguation_page_titles():
+    all_disambiguation_links_ak = WIKI.page(
+        "Wikipedia:Links_to_(disambiguation)_pages/A-K"
+    ).links
+    all_disambiguation_links_lz = WIKI.page(
+        "Wikipedia:Links_to_(disambiguation)_pages/L-Z"
+    ).links
+
+    all_disambiguation_links = {
+        **all_disambiguation_links_ak,
+        **all_disambiguation_links_lz,
+    }
+
+    # ns (namespace) 0 corresponds to main/article content: https://en.wikipedia.org/wiki/Wikipedia:Namespace
+    return [title for title, page in all_disambiguation_links.items() if page.ns == 0]
+
+
 def load_checkpoint(filepath):
     """Load existing entities from a JSON checkpoint file."""
     if os.path.exists(filepath):
