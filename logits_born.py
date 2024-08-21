862daee75802774bfd50c7e1e087d0f23a891633
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-21 10:54:44 +0200

    renamed class for clarity

--- logits_born.py
+++ logits_born.py
@@ -5,9 +5,9 @@ from bornrule.torch import Born
 
 
 # check the born_layers.ipynb notebook to see why it's probably a good idea to use this layer as a hidden one, instead of the default Born layer
-class BornLogits(nn.Module):
+class LogitsBornLayer(nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
-        super(BornLogits, self).__init__()
+        super(LogitsBornLayer, self).__init__()
         self.born = Born(in_features, out_features, device=device, dtype=dtype)
 
     def forward(self, x):
@@ -26,7 +26,7 @@ class LogitsBorn(nn.Module):
         self.layers = nn.ModuleList()
         for i in range(len(layer_sizes) - 2):
             self.layers.append(
-                BornLogits(
+                LogitsBornLayer(
                     layer_sizes[i], layer_sizes[i + 1], device=device, dtype=dtype
                 )
             )
