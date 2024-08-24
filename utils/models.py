faa7943c02bffc1933e3a882fbbfdc76c9a116ef
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-24 15:01:58 +0200

    added mixture classifier as a candidate for further experimentation

--- utils/models.py
+++ utils/models.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 import numpy as np
 
+from sklearn.base import BaseEstimator, ClassifierMixin
+
 from typing import Literal
 
 
@@ -17,3 +19,23 @@ def get_latest_model(mtype: Literal["clf", "mlp"] = "clf"):
     )
 
     return load(model_files[latest])
+
+
+class ProbabilityMixtureClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, large_clf, small_clf, alpha=0.5):
+        self.large_clf = large_clf
+        self.small_clf = small_clf
+        self.weight1 = alpha
+
+    def fit(self, X, y):
+        # large_clf is assumed to be fitted
+        self.small_clf.fit(X, y)
+        return self
+
+    def predict_proba(self, X):
+        prob1 = self.large_clf.predict_proba(X)
+        prob2 = self.small_clf.predict_proba(X)
+        return self.alpha * prob1 + (1 - self.alpha) * prob2
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
