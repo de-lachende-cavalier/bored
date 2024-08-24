40c3db9dee024d1424d017385055effe92ad0005
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-24 16:50:32 +0200

    added util to construct mlp dataset

--- utils/models.py
+++ utils/models.py
@@ -3,22 +3,12 @@ from pathlib import Path
 from datetime import datetime
 
 import numpy as np
+import pandas as pd
+import torch
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 from typing import Literal
-
-
-def get_latest_model(mtype: Literal["clf", "mlp"] = "clf"):
-    path = Path("models/")
-    dt_format = "%d%m%Y-%H%M%S"
-
-    model_files = [str(file) for file in path.glob(f"{mtype}_*")]
-    latest = np.argmax(
-        [datetime.strptime(f.split("_")[-1], dt_format) for f in model_files]
-    )
-
-    return load(model_files[latest])
 
 
 class ProbabilityMixtureClassifier(BaseEstimator, ClassifierMixin):
@@ -39,3 +29,35 @@ class ProbabilityMixtureClassifier(BaseEstimator, ClassifierMixin):
 
     def predict(self, X):
         return np.argmax(self.predict_proba(X), axis=1)
+
+
+def get_latest_model(mtype: Literal["clf", "mlp"] = "clf"):
+    path = Path("models/")
+    dt_format = "%d%m%Y-%H%M%S"
+
+    model_files = [str(file) for file in path.glob(f"{mtype}_*")]
+    latest = np.argmax(
+        [datetime.strptime(f.split("_")[-1], dt_format) for f in model_files]
+    )
+
+    return load(model_files[latest])
+
+
+def get_mlp_data(X, y, born_clf, features, mappings):
+    columns = mappings["ner_tag"]
+
+    mlp_X = []
+    mlp_y = []
+    for i, x in enumerate(X):
+        x_explanation = pd.DataFrame(
+            born_clf.explain(x).toarray(), index=features, columns=columns
+        )
+        most_likely_y = x_explanation.sum().idxmax()
+
+        num_ner_tag = list(mappings["ner_tag"]).index(most_likely_y)
+        mlp_features = x_explanation[most_likely_y].to_list()
+        mlp_features.append(num_ner_tag)
+
+        mlp_X.append(mlp_features)
+        mlp_y.append(y[i])
+        return torch.tensor(mlp_X), torch.tensor(mlp_y)
